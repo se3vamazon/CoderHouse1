bff8bc9e01a0f9df8d74cfcd29fc796bb6f773f7
Sebastian Sampere <sebastiansampere@gmail.com>
2022-08-29 22:08:42 -0300

    Primer commit

---
+++ AppCoder/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+class Familiares(models.Model):
+    nombre = models.CharField(max_length=20)
+    edad = models.IntegerField()
+    altura = models.IntegerField()
+    fecha = models.DateField()
+
---
+++ AppCoder/views.py
@@ -0,0 +1,21 @@
+from ast import AsyncFunctionDef
+from django.shortcuts import render
+from AppCoder.models import Familiares
+import datetime
+
+# Create your views here.
+
+def viewfamiliares(request):
+    familiar1 = Familiares(nombre= 'Seba', edad = 29, altura= 1.77, fecha = datetime.datetime.now())
+    familiar2 = Familiares(nombre= 'Emma', edad = 29, altura = 1.80, fecha = datetime.datetime.now())
+    familiar3 = Familiares(nombre = 'Andy', edad =  26, altura = 1.80, fecha = datetime.datetime.now())
+    familiar1.save()
+    familiar2.save()
+    familiar3.save()
+
+    contexto = {
+        'familiar1': familiar1,
+        'familiar2' : familiar2,
+        'familiar3' : familiar3,
+    }
+    return render(request, 'familia.html', contexto)
